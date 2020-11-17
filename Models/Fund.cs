bb6e5f3de05ce8e8aa347d04261d07254fb2b0d7
unknown <alia.azarian@gmail.com>
2020-11-17 12:48:26 +0000

    Class 'Fund' created in the Models folder

---
+++ Models/Fund.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;                // [Key]
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grids.Models
+{
+    public class Fund
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FundStructureId { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string LaunchDate { get; set; } = "";
+    }
+}
