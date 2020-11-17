39f939d958225f2820b93de02bd7b6226347398d
unknown <alia.azarian@gmail.com>
2020-11-17 13:00:40 +0000

    IService folder added

---
+++ IService/IFundService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grids.Models;
+
+namespace Grids.IService
+{
+    public interface IFundService
+    {
+       
+            List<Fund> GetFunds();
+            Fund GetById(int fundId);
+            void Save(Fund oFund);
+            void Update(Fund oFund);
+            string Delete(int fundId);
+       
+    }
+}
